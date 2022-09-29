908053e1dc584400b6ceb1df6258656cb9b66f39
Angela Khorana <112173502+Angelak731@users.noreply.github.com>
2022-09-28 19:33:42 -1000

    Add files via upload

--- Lab8/Ex3b.js
+++ Lab8/Ex3b.js
@@ -1,12 +1,9 @@
 const { count } = require("console");
 
 require("./products_data.js");
-var num_products = 5;
-var counter = 1;
 
-while (counter <= num_products) { 
+for (counter = 1);eval("typeof name"+i) != 'undefined'; counter++) {
     console.log(counter + "." + eval('name' + counter));
-    counter++;
 }
 
 console.log("That's all we have!");
