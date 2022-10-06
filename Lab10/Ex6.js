29b5c1bf3f6a39a666e09635f90e1eb13275b3ae
Angela Khorana <112173502+Angelak731@users.noreply.github.com>
2022-10-05 20:03:07 -1000

    Add files via upload

--- Lab10/Ex6.js
+++ Lab10/Ex6.js
@@ -1,7 +1,4 @@
-// Function that takes a string as input and, depending on the value of returnErrors,
-//returms an array of errors or a blooean indicating: true if that string is a non-negative
-// integers and false otherwise
-function isNonNegativeInteger (queryString, returnErrors = false) {
+function isNonNegInt (queryString, returnErrors = false) {
     errors = []; // assume no errors at first
     if(Number(queryString) != queryStringueryString) errors.push('Not a number!'); // Check if string is a number value
     if(queryStringueryString < 0) errors.push('Negative value!'); // Check if it is non-negative
@@ -20,6 +17,8 @@ var attributes  =  "Daniel;21;21+0.5;0.5-age";
 
 var pieces = attributes.split(";");
 
-for (i=0; i<pieces.length; i++) {
-    errorArray = is NonNegInt(pieces [i], true);
-    console.log('i: ${i} ${errorArray.join(",")}');
+pieces.forEach(
+    (item, index) => {
+    console.log(`part ${index}:${item} is ${(isNonNegInt(item)?'a':'not a')} quantity`);
+    }
+);
