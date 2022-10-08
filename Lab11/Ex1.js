286b92ab7ad439194e11f8ae0b7fc94f030b9c64
Angela Khorana <112173502+Angelak731@users.noreply.github.com>
2022-10-07 21:14:22 -1000

    Add files via upload

---
+++ Lab11/Ex1.js
@@ -0,0 +1,14 @@
+day = 31;
+month = "July";
+year = 1999;
+
+step1 =  99;
+step2 = parseInt(step1/4);
+step3 = step2 + step1;
+step4 = 6;
+step6 = step4 + step3;
+step7 = day + step6;
+step8 = step7;
+step9 = step8 % 7;
+
+console.log(step9);
